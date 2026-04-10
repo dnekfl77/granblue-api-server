5dd72ce579d52850b83410e975c11fcfa0c694e3
장진희 <jhjang@ensmart.co.kr>
2026-04-10 18:02:15 +0900

    Swagger 설정파일 추가

---
+++ src/main/java/com/granblue/api/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.granblue.api.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwtSchemeName = "jwtAuth";
+
+        // API 요청 헤더에 인증 정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        // SecurityScheme 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Granblue API Server")
+                        .description("Granblue 프로젝트 REST API 문서")
+                        .version("v1.0.0"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
